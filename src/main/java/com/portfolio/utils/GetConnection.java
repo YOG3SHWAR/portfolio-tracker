59fa4cd60a1196c73b1dea9d88e3a681b8a08b5f
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-06-01 21:40:45 +0530

    Database Connection

---
+++ src/main/java/com/portfolio/utils/GetConnection.java
@@ -0,0 +1,41 @@
+package com.portfolio.utils;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class GetConnection {
+	private static String driver;
+	private static String url;
+	private static String username;
+	private static String password;
+
+	public static Connection getMySQLConn() {
+		ResourceBundle rb = ResourceBundle.getBundle("db");
+		driver = rb.getString("driver");
+		url = rb.getString("url");
+		username = rb.getString("username");
+		password = rb.getString("password");
+
+		try {
+			Class.forName(driver);
+			return DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+	public static PreparedStatement getPreparedStatement(String sqlString) {
+		try {
+			return getMySQLConn().prepareStatement(sqlString);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
